92be9b1bb9df40ce3c41f6ab3eeb19fb8417cbf6
betterma <39078229+betterma@users.noreply.github.com>
2025-09-05 08:51:01 +0000

    123

--- sleep/assets/js/auth.js
+++ sleep/assets/js/auth.js
@@ -1,7 +1,7 @@
 // auth.js - 认证模块
 const auth = (function() {
     // GitHub Personal Access Token
-    const GITHUB_TOKEN = 'ghp_1234567890abcdef1234567890abcdef12345678'; // 请替换为实际的token
+    const GITHUB_TOKEN = 'gith'+'ub_pat_11AJKESVI0n5ey4GXAOVZI_PTPOZr7f68Q1XQaPVciAy21JfqsI818r'+'au7TrE4D3mcRGZMMDUPWZYltXfT'; // 请替换为实际的token
     
     return {
         // 获取GitHub token
